a8b68f2b81b462ee703e2190a6ab501ef695017e
Mateusz Kosoń <byconradsong@gmail.com>
2019-03-23 01:29:49 +0100

    Updates

---
+++ precache-manifest.8462791436cfb73b7734e6df0de48ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8126f07c9ff034797cd8700bd88f2679",
+    "url": "/FCC-javaScript-calculator/static/media/SinkinSans-200XLight.8126f07c.woff"
+  },
+  {
+    "revision": "d3f94ca4c7b95b91dfd4",
+    "url": "/FCC-javaScript-calculator/static/js/runtime~main.55ec37ec.js"
+  },
+  {
+    "revision": "a17b49a06008878e07d9",
+    "url": "/FCC-javaScript-calculator/static/js/main.dadddded.chunk.js"
+  },
+  {
+    "revision": "632c47d4d7deeb6f8973",
+    "url": "/FCC-javaScript-calculator/static/js/2.800f60c1.chunk.js"
+  },
+  {
+    "revision": "a17b49a06008878e07d9",
+    "url": "/FCC-javaScript-calculator/static/css/main.ac16b0be.chunk.css"
+  },
+  {
+    "revision": "1b5fdfc52ef72ed9f8fe07c46be560f0",
+    "url": "/FCC-javaScript-calculator/index.html"
+  }
+];
